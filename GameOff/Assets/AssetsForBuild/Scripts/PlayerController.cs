c3a1bbbfe57abb37790221e7fadc955b19126a8f
Ayan <>
2020-11-03 01:02:27 +0530

    Player movement - Basic setup

--- GameOff/Assets/AssetsForBuild/Scripts/PlayerController.cs
+++ GameOff/Assets/AssetsForBuild/Scripts/PlayerController.cs
@@ -2,17 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerInputHandler))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float m_MoveSpeed;
+    [SerializeField] private float m_JumpForce;
+    [SerializeField] private float m_JumpBreakForce;
+    [SerializeField] private float m_gravityScale;
+
+    private Vector3 _moveDir;
+
+    private CharacterController controller;
+    private PlayerInputHandler playerInput;
+
+    private void Awake()
     {
-        
+        controller = this.GetComponent<CharacterController>();
+        playerInput = this.GetComponent<PlayerInputHandler>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
     }
+
+    void Move()
+    {
+        _moveDir = new Vector3(playerInput.InputVector.x * m_MoveSpeed, _moveDir.y, playerInput.InputVector.y * m_MoveSpeed);
+
+        if (controller.isGrounded)
+        {
+            if (playerInput.Jump)
+            {
+                _moveDir.y = m_JumpForce;
+            }
+        }
+        else
+        {
+            if (playerInput.JumpBreak)
+            {
+                _moveDir.y *= m_JumpBreakForce;
+            }
+        }
+
+        _moveDir.y = _moveDir.y + (Physics.gravity.y * m_gravityScale * Time.deltaTime);
+
+        controller.Move(_moveDir * Time.deltaTime);
+    }
+
+
 }
---
+++ GameOff/Assets/AssetsForBuild/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    public Vector2 InputVector { get; private set; }
+
+    public bool Jump { get; private set; }
+
+    public bool JumpBreak { get; private set; }
+
+    private float _horizontal;
+    private float _vertical;
+
+    private void Update()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+
+        InputVector = new Vector2(_horizontal, _vertical);
+
+        Jump = Input.GetButtonDown("Jump");
+
+        JumpBreak = Input.GetButtonUp("Jump");
+
+
+    }
+}
